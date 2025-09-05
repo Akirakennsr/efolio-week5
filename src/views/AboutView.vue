4a921b8ca6e6bbdc2feb867318546791fe4c342d
Akiraken <qianzehao01@gmail.com>
2025-09-05 21:57:59 +1000

    update the same css

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="about">
-    <h1>About Our Library</h1>
-    <p>Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
+  <div class="container mt-4">
+    <h1 class="text-center mb-3">About Our Library</h1>
+    <p class="text-center">Welcome to our digital library! We're dedicated to providing a vast collection of books and resources to our community.</p>
   </div>
 </template>
 

56ee7e8b6b719c39635a073d179d7a683fcd4f04
Akiraken <qianzehao01@gmail.com>
2025-09-19 14:11:31 +1000

    update efolio task 7.1

--- src/main.js
+++ src/main.js
@@ -17,3 +17,23 @@ app.use(router)
 // app.component('Column', Column)
 
 app.mount('#app')
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB9sW4WtSco4aMZO9BmzVQc_Eb5w5-jqeo",
+  authDomain: "week7-zehaoqian.firebaseapp.com",
+  projectId: "week7-zehaoqian",
+  storageBucket: "week7-zehaoqian.firebasestorage.app",
+  messagingSenderId: "135739415780",
+  appId: "1:135739415780:web:468eb8255788fe48d020f1"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
